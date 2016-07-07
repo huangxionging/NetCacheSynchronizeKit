f5f73b27f643f610b0af731aa12839102729b1a8
huangxiong <huangxionging@163.com>
2016-07-07 15:49:31 +0800

    Initial Commit

---
+++ NetCacheSynchronizeKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetCacheSynchronizeKit
+//
+//  Created by huangxiong on 16/7/7.
+//  Copyright © 2016年 huangxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
